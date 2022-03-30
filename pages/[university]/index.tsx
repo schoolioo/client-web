e824d6146690d710c050377404e168ac837d7287
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-03-30 13:58:11 +0200

    feat: begging university page

--- pages/[university]/index.tsx
+++ pages/[university]/index.tsx
@@ -1,7 +1,14 @@
-export const UniversityIndexPage = () => {
+import useSWR from "swr";
+import fetcher from "../../utils/fetcher";
+
+const UniversityIndexPage = () => {
+  const { data, error } = useSWR("http://schooliu.ddns.net:3000/universities?page=1&numberPerPage=50", fetcher);
+
   return (
     <div>
-
+      {JSON.stringify(data)}
     </div>
   );
 };
+
+export default UniversityIndexPage
